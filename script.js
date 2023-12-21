0b19d9b5d23ff4e09c6fca53b19eca5430965855
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2023-12-21 16:57:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let temp=str.split("").sort().join("");
+	
+	console.log(temp);
+	for(let i=0;i<temp.length;i++){
+		if(i==0 && temp[i]!==temp[i+1] || i==temp.length-1 && temp[i]!==temp[i-1] || temp[i]!==temp[i-1] && temp[i]!==temp[i+1])return temp[i];
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
